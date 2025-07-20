a8b566c7c84f8f336b445bc8bff849300830d3a1
mirzasohbanbaig <mirza99707@gmail.com>
2025-07-20 08:18:54 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Quiz from './Components/Quiz/Quiz'
+
+const App = () => {
+  return (
+    <>
+      <Quiz/>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
+
